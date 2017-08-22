feb84c621020cf290e44b3898564afbf26f100b3
John Blossom <jblossom.web@gmail.com>
2017-08-21 23:59:42 -0600

    tour of heroes: step 1 and 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
+
+let title = 'Tour of Heroes';
+
+class Hero {
+  id: number;
+  name: string;
+}
+let Windstorm: Hero = {id: 1, name: 'Windstorm'}
+let hero = Windstorm;
+
+const HEROES: Hero[] = [
+  Windstorm,
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [
         AppComponent
       ],
@@ -17,16 +41,23 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title '${title}'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual(title);
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it(`should render title in a h1 tag 'Welcome to ${title}!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain(`Welcome to ${title}!`);
   }));
+
+  // it(`should render hero in 2nd h2 tag '${hero.name} details'`, async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h2:nth-of-type(2)').textContent).toContain(`${hero.name} details`);
+  // }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+let Windstorm: Hero = {id: 1, name: 'Windstorm'}
+
+const HEROES: Hero[] = [
+  Windstorm,
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title: string = 'Tour of Heroes';
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero; // = Windstorm;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }
